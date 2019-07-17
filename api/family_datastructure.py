805b1b86ae919de579422b78252b80bfdf169bb3
MageLnk <mage.link.zhezard@gmail.com>
2019-07-17 16:57:55 +0000

    ola

--- api/family_datastructure.py
+++ api/family_datastructure.py
@@ -14,27 +14,63 @@ class Family:
         # example list of members
         self._members = [{
             "id": 1,
-            "first_name": "John"
-        }]
+            "first_name": "John",
+            "last_name": "Doe",
+            "age": "33 Years old",
+            "gender": "Male",
+            "Lucky_numbers": ["7", "13", "22"]
+        },
+        {
+            "id": 2,
+            "first_name": "Jane",
+            "last_name": "Doe",
+            "age": "35 Years old",
+            "gender": "Female",
+            "Lucky_numbers": ["10", "14", "3"]
+        }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
-        # fill this method and update the return
+        for i in self._members:
+            if id==i["id"]:
+                Informacion = {
+                    id: self._generateId(),
+                    first_name:member["first_name"],
+                    last_name:member["Doe"],
+                    age:member["age"],
+                    gender:member["gender"],
+                    lucky_numbers:member["lucky_numbers"]
+                }
+                self._members.append(Informacion)
         return None
 
     def delete_member(self, id):
-        # fill this method and update the return
+        for i in self._members:
+            if id==i["id"]:
+                self._members.remove(i)
+                return ('Done')
         return None
 
-    def update_member(self, id, member):
-        # fill this method and update the return
+    def update_member(self, id, member, request):
+        for i in self._members:
+            if id==i["id"]:
+                Informacion = {
+                    first_name:request.data["first_name"],
+                    age:request.data["age"],
+                    gender:request.data["gender"],
+                    lucky_numbers:request.data["lucky_numbers"]
+                }
+                self._members.append(Informacion)
         return None
 
     def get_member(self, id):
-        # fill this method and update the return
+        for i in self._members:
+            if id==i["id"]:
+                return i
         return None
 
     # this method is done, it returns a list with all the family members
--- api/views.py
+++ api/views.py
@@ -16,19 +16,30 @@ The MembersView will contain the logic on how to:
 class MembersView(APIView):
     def get(self, request, member_id=None):
         # fill this method and update the return
-        result = None
-        return Response(result, status=status.HTTP_200_OK)
+        if member_id is not None:
+            zorro=family.get_member(member_id)
+            Response(zorro, status=status.HTTP_200_OK)
+        else:
+            zorro=family.get_all_members()
+        return Response(zorro, status=status.HTTP_400_BAD_REQUEST)
 
     def post(self, request):
-        # fill this method and update the return
+        alce=family.add_member(request)
         result = None
         return Response(result, status=status.HTTP_200_OK)
 
     def put(self, request, member_id=None):
-        # fill this method and update the return
-        result = None
-        return Response(result, status=status.HTTP_200_OK)
+        if member_id is not None:
+            mapache=family.update_member(member_id, request.data)
+            Response(mapache, status=status.HTTP_200_OK)
+        else:
+            mapache=family.get_all_members()
+        return Response(mapache, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, member_id=None):
-        # fill this method and update the return
+        if member_id is not None:
+            lobo=family.delete_member(member_id)
+            Response(lobo, status=status.HTTP_200_OK)
+        else:
+            return Response([], status=status.HTTP_400_BAD_REQUEST)
         return Response({ "status": "ok" }, status=status.HTTP_200_OK)
